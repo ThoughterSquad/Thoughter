b50161022a6968527b7ed1d4a929d5aa0849bd07
Christine Ash <ChristineTheBrave@Christines-MacBook-Pro.local>
2017-03-10 11:10:37 -0500

    Added changes to new thought js file

--- src/new-thought.js
+++ src/new-thought.js
@@ -1,4 +1,23 @@
 (function newThoughtModule() {
   'use strict';
     window.thoughter = window.thoughter || {};
+
+    let promise = fetch(
+  'https://thoughter.herokuapp.com/api',
+  {
+      method: 'POST',
+      headers: {
+        Authorization: 'token ' + ''
+      },
+      body: {
+        JSON.stringify
+      }
+
+  }
+});
+
+
+
+
+
 }());
