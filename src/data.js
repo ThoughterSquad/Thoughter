c4b70b51b65448a8906a524d35eea094e8b7e236
Nadia Mughal <nmughal14@gmail.com>
2017-03-11 15:35:00 -0500

    created a forEach function to log out individual recent thoughts

--- src/data.js
+++ src/data.js
@@ -2,24 +2,32 @@
   'use strict'; //use Javascript exactly how it's written
 
     window.thoughter = window.thoughter || {};
-
-    console.log('data module');
-
     window.thoughter.retrieveThoughts = function retrieveThoughts() {
       //Fetch request for thoughts
-      let promise = fetch('https://thoughter.herokuapp.com/api/Thoughts', {
+      fetch('https://thoughter.herokuapp.com/api/Thoughts?filter={"limit":20, "order":"createTime DESC"}', {
         method: 'GET'
       })
       .then(function handleResponse(responseObj) {
-        console.log(responseObj.status);// check for response status
         if (responseObj.status > 199 && responseObj.status < 300) {
           responseObj.json().then(function printData(data) {
-            console.log('All thoughts!', data);
+            data.forEach(function logThought(thoughtObj) {
+              let thoughtObjUl = document.querySelector('.recentthoughts-section');
+              let thoughtObjLi = document.createElement('li');
+              thoughtObjLi.setAttribute('class','recent-thought');
+              let thoughtHeader = document.createElement('h5');
+              thoughtHeader.setAttribute('class', 'alert alert-info');
+              thoughtHeader.innerText = thoughtObj.createTime;
+              let thoughtContent = document.createElement('p');
+              thoughtContent.innerText = thoughtObj.content;
+              thoughtObjLi.appendChild(thoughtHeader);
+              thoughtObjLi.appendChild(thoughtContent);
+              thoughtObjUl.appendChild(thoughtObjLi);
+              console.log(thoughtObjUl);
+            });
           });
         }
       });
     };
 
     window.thoughter.retrieveThoughts();
-
 }) ();
