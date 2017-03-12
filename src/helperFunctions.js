099145552182a6086218f534857af9eae35dbfda
Brian Fiala <bgfiala@gmail.com>
2017-03-12 16:34:00 -0400

    renamed data.js to be more representative: recentthoughts.js and extracted a code block for building a thought html list item into a helper function

--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -12,6 +12,20 @@
     return formattedDate;
   };
 
+  // creates a thought list item with header and content
+  window.thoughter.displayThought = function displayThought(thoughtObj) {
+    let thoughtObjLi = document.createElement('li');
+    thoughtObjLi.setAttribute('class','recent-thought');
+    let thoughtHeader = document.createElement('h5');
+    thoughtHeader.setAttribute('class', 'alert alert-info');
+    thoughtHeader.innerText = window.thoughter.formatThoughtHeader(new Date(thoughtObj.createTime));
+    let thoughtContent = document.createElement('p');
+    thoughtContent.innerText = thoughtObj.content;
+    thoughtObjLi.appendChild(thoughtHeader);
+    thoughtObjLi.appendChild(thoughtContent);
+    return thoughtObjLi;
+  };
+
   // function used with thought creation date and author username to format a header for thought display
   window.thoughter.formatThoughtHeader = function formatThoughtHeader(date)
     // , username) // commented out until username functionality implemented
--- src/new-thought.js
+++ src/new-thought.js
@@ -4,10 +4,11 @@
 
     // POSTs a new thought to the backend database
     window.thoughter.postThought = function postThought(newThoughtContent) {
+      let newThoughtObject = {"content": newThoughtContent, "authorId": window.localStorage.userId};
       fetch('https://thoughter.herokuapp.com/api/Thoughts', {
           method: 'POST',
           headers: {'content-type': 'application/json'},
-          body: JSON.stringify({"content": newThoughtContent, "authorId": window.localStorage.userId})
+          body: JSON.stringify(newThoughtObject)
         }
       ).then(function verifyNewThougthPost(response) {
         if (response.status > 199 && response.status < 300) {
@@ -19,6 +20,10 @@
         }
       });
     };
+
+    window.thoughter.displayNewThought = function displayNewThought(newThoughtObject) {
+      
+    }
 
     $('.newthought-form').on('submit', function loginEventHandler(e) {
       e.preventDefault();
