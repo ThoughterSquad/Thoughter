4ca7710268cd579aac0ad5858ca7d72ec0c207e4
Brian Fiala <bgfiala@gmail.com>
2017-03-12 13:50:31 -0400

    beginning work on response handler for posting new thought, added function stub for new thought submission event handling

--- src/new-thought.js
+++ src/new-thought.js
@@ -1,16 +1,23 @@
-(function newThoughtModule() {
+(function () {
   'use strict';
     window.thoughter = window.thoughter || {};
 
+    // POSTs a new thought to the backend database
     function postThought(thought) {
       fetch('https://thoughter.herokuapp.com/api/Thoughts', {
           method: 'POST',
-          headers: {
-            'content-type': 'application/json'
-          },
+          headers: {'content-type': 'application/json'},
           body: JSON.stringify(thought)
         }
-      );
+      ).then(function verifyNewThougthPost(data) {
+
+      });
     }
+
+    $('.login-form').on('submit', function loginEventHandler(e) {
+      e.preventDefault();
+      window.localStorage.setItem('username', $('#login-username').val());
+      window.thoughter.loginUser($('#login-password').val());
+    });
 
 }());
